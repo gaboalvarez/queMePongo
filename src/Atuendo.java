d6aada6478ab73f9ad3ff2712e5d2ea336289463
gaboalvarez <alvarezgabriel907@gmail.com>
2020-04-16 10:17:23 -0300

    segundo commit, mejoro la expresividad

--- src/Atuendo.java
+++ src/Atuendo.java
@@ -3,12 +3,12 @@ import java.util.List;
 
 public class Atuendo {
     List<Prenda> prendas = new ArrayList<>();
-    int sup = 0,inf = 0,cal = 0;
+    int superior = 0,inferior = 0,calzado = 0;
     void agregarPrenda(Prenda prenda){
-        if(prenda.getCat()=="sup" && sup==0){sup+=1;prendas.add(prenda);}
-        if(prenda.getCat()=="inf" && inf==0){inf+=1;prendas.add(prenda);}
-        if(prenda.getCat()=="cal" && cal==0){cal+=1;prendas.add(prenda);}
-        if(prenda.getCat()=="acc"){prendas.add(prenda);}
+        if(prenda.getCategoria()=="superior" && superior==0){superior+=1;prendas.add(prenda);}
+        if(prenda.getCategoria()=="inferior" && inferior==0){inferior+=1;prendas.add(prenda);}
+        if(prenda.getCategoria()=="calzado" && calzado==0){calzado+=1;prendas.add(prenda);}
+        if(prenda.getCategoria()=="accesorio"){prendas.add(prenda);}
     }
 // en esta solucion, se van agregando las prendas de a una, y no se admiten categorias repetidas (no puede haber 2 calzados, o 2 partes superiores).
 }
--- src/Prenda.java
+++ src/Prenda.java
@@ -3,29 +3,33 @@ public class Prenda {
     String material;
     String colorPrincipal, colorSecundario;
     Prenda(Categoria categoria, String material, String colorPrincipal){
-        this.categoria = categoria;this.material = material;this.colorPrincipal = colorPrincipal;}
+        this.categoria = categoria;this.material = material;this.colorPrincipal = colorPrincipal;
+
+    }
     Prenda(Categoria categoria, String material, String colorPrincipal, String colorSecundario){
-        this.categoria = categoria;this.material = material;this.colorPrincipal = colorPrincipal;this.colorSecundario = colorSecundario;}
-    String getCat(){return categoria.getCat();}
+        this.categoria = categoria;this.material = material;this.colorPrincipal = colorPrincipal;this.colorSecundario = colorSecundario;
+
+    }
+    String getCategoria(){return categoria.getCategoria();}
 }
-interface Categoria{String getCat();}
+interface Categoria{String getCategoria();}
 class ParteSuperior implements Categoria{
-    String tipo,cat;
-    ParteSuperior(String tipo){this.tipo = tipo;this.cat="sup";}
-    public String getCat(){return cat;}
+    String tipo,categoria;
+    ParteSuperior(String tipo){this.tipo = tipo;this.categoria="superior";}
+    public String getCategoria(){return categoria;}
 }
 class ParteInferior implements Categoria{
-    String tipo,cat;
-    ParteInferior(String tipo){this.tipo = tipo;this.cat="inf";}
-    public String getCat(){return cat;}
+    String tipo,categoria;
+    ParteInferior(String tipo){this.tipo = tipo;this.categoria="inferior";}
+    public String getCategoria(){return categoria;}
 }
 class Calzado implements Categoria{
-    String tipo,cat;
-    Calzado(String tipo){this.tipo = tipo;this.cat="cal";}
-    public String getCat(){return cat;}
+    String tipo,categoria;
+    Calzado(String tipo){this.tipo = tipo;this.categoria="calzado";}
+    public String getCategoria(){return categoria;}
 }
 class Accesorio implements Categoria{
-    String tipo,cat;
-    Accesorio(String tipo){this.tipo = tipo;this.cat="acc";}
-    public String getCat(){return cat;}
+    String tipo,categoria;
+    Accesorio(String tipo){this.tipo = tipo;this.categoria="accesorio";}
+    public String getCategoria(){return categoria;}
 }
