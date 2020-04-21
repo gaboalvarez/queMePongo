7a70e3b7c7f1fd51f02097d8b1c7e06ed3a52860
gaboalvarez <alvarezgabriel907@gmail.com>
2020-04-21 16:25:53 -0300

    termino 1er iteracion, arreglado ejemplos de main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Prenda remeraRoja = new Prenda(new TipoPrenda("remera",new ParteSuperior()),"algodon","rojo");
         System.out.println("prueba1");
-        Prenda pantalonAzul = new Prenda(new TipoPrenda("pantalon",new ParteSuperior()),"algodon","azul");
+        Prenda pantalonAzul = new Prenda(new TipoPrenda("pantalon",new ParteInferior()),"algodon","azul");
         System.out.println("prueba2");
+        Atuendo outfit1 = new Atuendo();
+        outfit1.agregar(remeraRoja);
+        outfit1.agregar(pantalonAzul);
+        System.out.println(outfit1.mostrarPrendas());
     }
 }
